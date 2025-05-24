351ffbafb60240dcd7f298ee4781209cca9cb0fa
Kenneth Shaw <kenshaw@gmail.com>
2025-05-25 05:59:42 +0700

    Fixing api

--- ffcookies.go
+++ ffcookies.go
@@ -12,11 +12,16 @@ import (
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
+	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/kenshaw/ffcookies/models"
 	"golang.org/x/net/publicsuffix"
 )
+
+// DefaultOpenParams are the default open parameters to use.
+var DefaultOpenParams = "?nolock=1&immutable=1&mode=ro"
 
 /*
 
@@ -76,6 +81,25 @@ func ReadFile(file, host string) ([]*http.Cookie, error) {
 	return ReadFileContext(context.Background(), file, host)
 }
 
+// ReadContext reads the cookies for the provided Firefox profile name, or the
+// default Firefox profile.
+func ReadContext(ctx context.Context, profile, host string) ([]*http.Cookie, error) {
+	profileDir := profileDir()
+	if profileDir == "" {
+		return nil, errors.New("cannot determine the firefox profile directory")
+	}
+	cookiePath, err := cookiePath(profileDir, profile)
+	if err != nil {
+		return nil, err
+	}
+	return ReadFileContext(ctx, "file:"+cookiePath+DefaultOpenParams, host)
+}
+
+// Read reads the cookies for the provided Firefox profile name.
+func Read(profile, host string) ([]*http.Cookie, error) {
+	return ReadContext(context.Background(), profile, host)
+}
+
 // Jar builds a cookie jar for the url from provided cookies.
 func Jar(u *url.URL, cookies ...*http.Cookie) (http.CookieJar, error) {
 	// build jar
@@ -91,7 +115,7 @@ func Jar(u *url.URL, cookies ...*http.Cookie) (http.CookieJar, error) {
 
 // ReadJarContext reads the cookies from the provided sqlite3 file for the provided
 // url into a cookie jar usable with http.Client.
-func ReadJarContext(ctx context.Context, file, urlstr string) (http.CookieJar, error) {
+func ReadJarContext(ctx context.Context, profile, urlstr string) (http.CookieJar, error) {
 	// read cookies
 	u, err := url.Parse(urlstr)
 	if err != nil {
@@ -102,17 +126,23 @@ func ReadJarContext(ctx context.Context, file, urlstr string) (http.CookieJar, e
 	default:
 		return nil, fmt.Errorf("invalid url scheme %q", u.Scheme)
 	}
-	cookies, err := ReadFileContext(ctx, file, u.Host)
+	cookies, err := ReadContext(ctx, profile, u.Host)
 	if err != nil {
 		return nil, err
 	}
 	return Jar(u, cookies...)
 }
 
+// ReadJar reads the cookies from the provided sqlite3 file for the provided
+// url into a cookie jar usable with http.Client.
+func ReadJar(profile, urlstr string) (http.CookieJar, error) {
+	return ReadJarContext(context.Background(), profile, urlstr)
+}
+
 // ReadJarFilteredContext reads the cookies from the provided sqlite3 file for
 // the provided url into a cookie jar (usable with http.Client) consisting of
 // cookies passed through filter func f.
-func ReadJarFilteredContext(ctx context.Context, file, urlstr string, f func(*http.Cookie) bool) (http.CookieJar, error) {
+func ReadJarFilteredContext(ctx context.Context, profile, urlstr string, f func(*http.Cookie) bool) (http.CookieJar, error) {
 	// read cookies
 	u, err := url.Parse(urlstr)
 	if err != nil {
@@ -123,7 +153,7 @@ func ReadJarFilteredContext(ctx context.Context, file, urlstr string, f func(*ht
 	default:
 		return nil, fmt.Errorf("invalid url scheme %q", u.Scheme)
 	}
-	cookies, err := ReadFileContext(ctx, file, u.Host)
+	cookies, err := ReadContext(ctx, profile, u.Host)
 	if err != nil {
 		return nil, err
 	}
@@ -137,6 +167,13 @@ func ReadJarFilteredContext(ctx context.Context, file, urlstr string, f func(*ht
 	return Jar(u, c...)
 }
 
+// ReadJarFiltered reads the cookies from the provided sqlite3 file for the
+// provided url into a cookie jar (usable with http.Client) consisting of
+// cookies passed through filter func f.
+func ReadJarFiltered(ctx context.Context, profile, urlstr string, f func(*http.Cookie) bool) (http.CookieJar, error) {
+	return ReadJarFilteredContext(context.Background(), profile, urlstr, f)
+}
+
 // driverName returns the first sqlite3 driver name it encounters.
 func driverName() string {
 	for _, n := range sql.Drivers() {
@@ -146,4 +183,29 @@ func driverName() string {
 		}
 	}
 	return ""
+}
+
+// profileDir returns the base profile directory for firefox.
+func profileDir() string {
+	if dir, err := os.UserHomeDir(); err == nil {
+		return filepath.Join(dir, ".mozilla", "firefox")
+	}
+	return ""
+}
+
+// cookiePath determines the cookie file path.
+func cookiePath(dir, profile string) (string, error) {
+	if profile == "" {
+		entries, err := os.ReadDir(dir)
+		if err != nil {
+			return "", err
+		}
+		for _, entry := range entries {
+			if name := entry.Name(); entry.IsDir() && strings.HasSuffix(name, ".default-release") {
+				dir = filepath.Join(dir, name)
+				break
+			}
+		}
+	}
+	return filepath.Join(dir, profile, "cookies.sqlite"), nil
 }
